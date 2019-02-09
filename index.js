29ebf9c8efd4b1789981bbdae5b1526fa6be5d7f
Emil Dahlberg <emmada@utu.fi>
2019-02-09 23:56:53 +0200

    partial update to work on later

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const fs = require('fs');
+
 const app = express();
 
+request('http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv').pipe(fs.createWriteStream('pop.csv'));
+
+// Create db connection
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+/*
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+*/
+
+// Serve static website at client/build/...
 app.use( express.static(path.join(__dirname, 'client/build') ));
 
 app.get( '/api/countries', (req, res) => {
@@ -15,5 +42,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
 
 console.log("listening on port " + port)
