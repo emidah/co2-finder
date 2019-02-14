99e85bc46c62c1128f2cf6c9e4f78e5cab4b228b
Emil Dahlberg <emil.m.dahlberg@gmail.com>
2019-02-15 00:48:58 +0200

    improved license footer

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ const Options = (props) => (
 
 const Footer = (props) => (
   <div>
-    <p>This website uses <a href="https://data.worldbank.org/">World Bank</a> <a href="https://data.worldbank.org/indicator/EN.ATM.CO2E.KT">CO2 emissions</a> and <a href="https://data.worldbank.org/indicator/SP.POP.TOTL">population</a> data under the Creative Commons 4.0 license.</p>
+    <p>This website uses <a href="https://data.worldbank.org/">World Bank</a> <a href="https://data.worldbank.org/indicator/EN.ATM.CO2E.KT">CO2 emissions</a> and <a href="https://data.worldbank.org/indicator/SP.POP.TOTL">population</a> data under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons 4.0 International license.</a></p>
   </div>
 )
 
